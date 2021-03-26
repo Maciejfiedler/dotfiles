92da6faa3e3a1a1bfdfebc5bcf9819b45ee73618
Maciej Fiedler <maciekfiedler@gmail.com>
2021-03-26 13:27:13 +0100

    xmonad.hs

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,45 @@
+import XMonad hiding ( (|||) )
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Layout.Maximize
+import XMonad.Layout.LayoutCombinators
+import System.IO
+
+myTerminal = "alacritty"
+myBrowser = "brave"
+
+myLayout = maximize $ avoidStruts (tiled ||| Mirror tiled) ||| Full
+  where tiled = Tall 1 (3/100) (1/2)
+
+main = do   
+    xmproc <- spawnPipe "xmobar" -- bar
+    -- trayer
+    spawnPipe "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --height 19 --alpha 150 --tint 0x00000"
+    spawnPipe "nitrogen --restore" --wallpaper
+    xmonad $ docks defaultConfig
+        { terminal = myTerminal
+        , focusedBorderColor = "#302d75"
+        , normalBorderColor = "#000000"
+        , borderWidth = 2
+        , layoutHook = myLayout
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppSep = " | "
+                        , ppTitle = xmobarColor "#d1e3ff" ""
+                        , ppLayout = xmobarColor "#679df0" "" 
+                        , ppCurrent = xmobarColor "#3c7ae6" "" . wrap "[" "]"
+                        , ppHidden = xmobarColor "#3b5e94" ""
+                        , ppHiddenNoWindows = xmobarColor "#142033" ""
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        }
+        `additionalKeysP` 
+        [ ("M-M1-<Return>", spawn myTerminal) 
+        , ("M-M1-b", spawn myBrowser) 
+        , ("M1-f", sendMessage $ JumpToLayout "Full")
+        , ("M1-r", withFocused (sendMessage . maximizeRestore))
+        , ("M1-w", kill)
+        , ("M1-p", spawn "rofi -show run -theme Monokai")
+        ]
